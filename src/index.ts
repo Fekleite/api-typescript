3acfab84adc511e923e1f1c853213132fda8ebff
Fekleite <fernandacarolineleite360@gmail.com>
2021-01-18 22:28:22 -0300

    [config] typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
 
-import routes from "./routes";
+import './database/connect';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
